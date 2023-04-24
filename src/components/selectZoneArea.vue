c0617cc95959c77faf8d90358c3f4a9549c1f928
louisnoiz <63070163@kmitl.ac.th>
2023-04-24 15:42:55 +0700

    update: ZoneArea page

--- src/components/selectZoneArea.vue
+++ src/components/selectZoneArea.vue
@@ -1,14 +1,54 @@
 <template>
-    <div class="container mx-auto grid-rows gap-4 flex justify-center items-center">
-    <section class="section" id="app">
-      <div class="content">
-      <div class="flex flex-wrap mt-4 ">
-        <div class="font-bold text-l mb-2">Select Zone Area</div>
-        
-      </div>
-    </div>
-    </section>
+    <div class="container mx-auto justify-center items-center">
+
+        <div class="grid grid-rows gap-4 border rounded-xl drop-shadow-lg border-[#c33838] px-20 mt-2">
+            <div class="flex py-8">
+                <div class="w-1/2">
+                    <div class="flex flex-col items-center">
+                        <img class="rounded-lg object-cover"
+                            src="https://scontent.fbkk7-3.fna.fbcdn.net/v/t1.6435-9/72731938_2574051899309409_6564049001522397184_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeHRdSCBhPVfSRHFD0ZnBqCa0ezs0S4-0IbR7OzRLj7Qhvb2QYZg8X5pJOOvK9hzplvmzlihcZKxYiNmUhZXgr2M&_nc_ohc=eZg8mzG0RC0AX9OTElM&_nc_ht=scontent.fbkk7-3.fna&oh=00_AfBknZju5JYrdTMQdLS254QUpjyCIjXLNJGmSC4MkOTEpQ&oe=646D9870"
+                            alt="">
+                    </div>
+                </div>
+                <div class="w-1/2 flex flex-col items-center">
+                    <div>
+                        <div class="font-bold text-xl mb-2">Concert's Zone Area details</div>
+                        <br>
+                        <div class="flex">
+                            <img class="colorZone"
+                                src="https://htmlcolorcodes.com/assets/images/colors/light-green-color-solid-background-1920x1080.png"
+                                alt="">
+                            <p>&ensp; Zone (Green) : 4,800 THB</p>
+                        </div>
+                        <br>
+                        <div class="flex">
+                            <img class="colorZone"
+                                src="https://htmlcolorcodes.com/assets/images/colors/sky-blue-color-solid-background-1920x1080.png"
+                                alt="">
+                            <p>&ensp; Zone (Blue) : 4,500 THB</p>
+                        </div>
+                        <br>
+                        <div class="flex">
+                            <img class="colorZone"
+                                src="https://htmlcolorcodes.com/assets/images/colors/periwinkle-color-solid-background-1920x1080.png"
+                                alt="">
+                            <p>&ensp; Zone (Purple) : 3,500 THB</p>
+                        </div>
+                        <br>
+                        <div class="flex">
+                            <img class="colorZone"
+                                src="https://htmlcolorcodes.com/assets/images/colors/burnt-sienna-color-solid-background-1920x1080.png"
+                                alt="">
+                            <p>&ensp; Zone (Orange) : 2,500 THB</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
 
-  </div>
-</template>
+</div></template>
+<style>.colorZone {
+    width: 20px;
+    height: 20px;
+}</style>
